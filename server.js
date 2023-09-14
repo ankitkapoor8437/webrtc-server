10e014f10ba66040cd88fadff7b46d74130a38f0
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-09-14 13:00:37 +0530

    Logic to handle the socket scenarios,

--- server.js
+++ server.js
@@ -14,16 +14,30 @@ const ServerPORT = 8001;
 app.use(bodyParser.json());
 
 const emailToSocketMapping = new Map();
+const socktetToemailMapping = new Map();
 
 io.on("connection", (socket) => {
     socket.on("join-room", (data) => {
         const { roomId, emailId } = data;
         console.log("User", emailId, "Joined Room", roomId);
         emailToSocketMapping.set(emailId, socket.id);
+        socktetToemailMapping.set(socket.id, emailId);
         socket.join(roomId);
         socket.emit("joined-room", { roomId });
         socket.broadcast.to(roomId).emit("user-joind", { emailId });
+    });
 
+    socket.on("call-user", (data) => {
+        const { emailId, offer } = data;
+        const fromEmail = socktetToemailMapping.get(socket.id);
+        const socketId = emailToSocketMapping.get(emailId);
+        socket.to(socketId).emit('incoming-call', { from: fromEmail, offer });
+    })
+
+    socket.on('call-accepted', (data) => {
+        const { emailId, ans } = data;
+        const socketId = emailToSocketMapping.get(emailId);
+        socket.to(socketId).emit('call-accepeted', { ans });
     })
 });
 
