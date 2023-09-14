93e85205653ceb50dd9093675f37f9bf66b3cd82
Ankit Kapoor <ankitkapoor8437@gmail.com>
2023-09-14 11:46:40 +0530

    Backend implimentation for Sockets

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { Server } = require("socket.io");
+const { Server, Socket } = require("socket.io");
 
 const app = express();
-const io = new Server();
+const io = new Server(
+    {
+        cors: true,
+    });
 
 const PORT = 8000;
 const ServerPORT = 8001;
 
 app.use(bodyParser.json());
-io.on("connection", (socket) => { });
+
+const emailToSocketMapping = new Map();
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (data) => {
+        const { roomId, emailId } = data;
+        console.log("User", emailId, "Joined Room", roomId);
+        emailToSocketMapping.set(emailId, socket.id);
+        socket.join(roomId);
+        socket.emit("joined-room", { roomId });
+        socket.broadcast.to(roomId).emit("user-joind", { emailId });
+
+    })
+});
 
 app.listen((PORT), () => {
     console.log(`Express server is running on ${PORT}`);
 });
 
-io.listen(8001);
+io.listen(ServerPORT);
